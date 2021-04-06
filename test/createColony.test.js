ce779198fa9aa7ab6d2acf6b9a56191d89cdd45c
Dan T <dp.tyler@hotmail.co.uk>
2021-04-06 15:47:55 +0100

    Test files tied to remove console logs)

--- test/createColony.test.js
+++ test/createColony.test.js
@@ -1,20 +1,18 @@
 import { DEFAULT_POPULATION } from "../src/Colony";
 import { createColony } from "../src/createColony";
 import { updatePopulation } from "../src/updatePopulation";
-import { Pedometer } from 'expo-sensors'
+import { Pedometer } from "expo-sensors";
 
-const STEPS_MISSED = 5
-const STEPS_HIT = 5000
+const STEPS_MISSED = 5;
+const STEPS_HIT = 5000;
 
-Pedometer.getStepCountAsync = jest.fn()
-Pedometer.isAvailableAsync = jest.fn()
+Pedometer.getStepCountAsync = jest.fn();
+Pedometer.isAvailableAsync = jest.fn();
 
 describe("createColony function", () => {
   dateToday = new Date();
   test("if date is 0, create new default colony", () => {
-    expect(createColony(0, 0, 0).showPopulation()).toEqual(
-      DEFAULT_POPULATION
-    );
+    expect(createColony(0, 0, 0).showPopulation()).toEqual(DEFAULT_POPULATION);
   });
   test("if date is greater than 7 days, create new population", () => {
     expect(createColony("2020-03-10", 5).showPopulation()).toEqual(
@@ -23,22 +21,18 @@ describe("createColony function", () => {
   });
   test("if the date is todays date, population stays the same", () => {
     expect(
-      createColony("2021-03-18", 1, [
-         { "name": "Emie",
-          "passion": "Writing music",
-          "personality": "Unsentimental",
-        },
-      ],"2021-03-18").showPopulation()
+      createColony(
+        "2021-03-18",
+        1,
+        [
+          {
+            name: "Emie",
+            passion: "Writing music",
+            personality: "Unsentimental",
+          },
+        ],
+        "2021-03-18"
+      ).showPopulation()
     ).toEqual(1);
   });
-  // test("if last Login is not today and less than 6 days ago", () => {
-  //   updatePopulation = jest.fn()
-  //   updatePopulation.mockReturnValue(true)
-  //   expect(createColony("2021-03-17", 1, [
-  //   { "name": "Emie",
-  //    "passion": "Writing music",
-  //    "personality": "Unsentimental",
-  //  },
-  //   ],"2021-03-18")).toEqual(true)
-  // });
 });
